c06537b13151f181cabc4c38f22b6bbd12d96288
Viktor Ivanov <viktiv02@gmail.com>
2023-10-14 04:49:30 +0200

    HttpClients Implemented !Tested
    HttpClients fully implemented with methods, but not tested!

---
+++ HttpClients/ClientInterfaces/IMediaService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.DTOs;
+using Domain.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IMediaService
+{
+    Task CreateAsync(MediaCreationDto dto);
+    Task<ICollection<Media>> GetAsync();
+    Task UpdateAsync(MediaUpdateDto dto);
+    Task DeleteAsync(int id);
+    Task<Media> GetById(int id);
+}
